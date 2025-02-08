06b8523b34fdb974175eec6186fc057c5c2829d9
watchfoxie <tocana.mar@gmail.com>
2025-02-08 16:22:57 +0200

    JS connect demo

---
+++ 1- Connecting JS/app.js
@@ -0,0 +1,12 @@
+// Basic script
+// Safe load and exe
+document.addEventListener('DOMContentLoaded', () => {
+    // Console congrads to devs
+    console.log('HTML doc is completly loaded and can be handled')
+    
+    // Traditional alert mess
+    alert('Hello world, i\'m JavaScript!')
+
+    // inner demonstration of congrads to users
+    document.getElementById('connection-state-container').innerHTML = "HTML document is completely loaded and can be manipulated";
+});
